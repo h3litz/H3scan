539cd9f30323419a9ce4af2915f29e2a1c977481
helit2 <hehe_24@163.com>
2017-01-11 00:50:22 +0800

    update crawl

--- spider/crawl.py
+++ spider/crawl.py
@@ -1,26 +1,39 @@
 __author__ = 'helit'
 import sys
+# import glob
+import os
+# import signal
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebKitWidgets import QWebPage
-# from xml import etree
 
-#use QtWebkit to get the final webpage
-class WebRender(QWebPage):
-    def __init__(self, url):
-        self.app = QApplication(sys.argv)
+class Crawler(QWebPage):
+    def __init__(self, url,file):
         QWebPage.__init__(self)
+        self._url = url
+        self._file = file
+
+    def crawl(self):
         self.loadFinished.connect(self.__loadFinished)
-        self.mainFrame().load(QUrl(url))
-        # self.app.platformName(minimal)
-        self.app.exec_()
+        print('载入网页..')
+        self.mainFrame().load(QUrl(self._url))
 
     def __loadFinished(self, result):
-        self.frame = self.mainFrame()
-        self.app.quit()
+        print('执行脚本..')
+        file = open(self._file,'w')
+        file.write(self.mainFrame().toHtml())
+        file.close()
+        sys.exit(0)
 
-url = 'http://demo.aisec.cn/demo/aisec/'
-r = WebRender(url)
-html = r.frame.toHtml()
-print(html)
+def main():
+    url = 'http://demo.aisec.cn/demo/aisec/'
+    print(sys.path[0])
+    file = os.path.join(sys.path[0], 'result/test.html')
+    app = QApplication(sys.argv)
+    crawler = Crawler(url,file)
+    crawler.crawl()
+    sys.exit(app.exec_())
 
+
+if __name__ == '__main__':
+    main()
